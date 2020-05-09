abeb907124cdb02e6f8431c136156b4b37932290
Daniela Yano <danielayano@Danielas-MacBook-Air.local>
2020-05-08 21:29:07 -0300

    First commit

---
+++ app.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 21 14:57:17 2020
+
+@author: etill
+"""
+
+#import statements
+from flask import Flask, render_template
+
+#Flask app variable
+app = Flask(__name__)
+
+#static route
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route("/courses")
+def courses():
+    return render_template('courses.html')
+#start the server
+if __name__ == "__main__":
+    app.run()
